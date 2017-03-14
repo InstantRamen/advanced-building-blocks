4984af9b71af0bc7ff1fe7f104eb798f887a8cb7
Derrick Cudd <instantramen125@gmail.com>
2017-03-14 21:17:52 +0000

    finish my_select method

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -10,4 +10,16 @@ module Enumerable
       yield self[i], i
     end
   end
+  
+  def my_select
+    # take as block that should return true or false
+    # sort through the items in self to and return an array of items for which the statement is true
+    new_array = []
+    self.my_each do |item|
+      if yield item
+        new_array.push item
+      end
+    end
+    return new_array
+  end
 end

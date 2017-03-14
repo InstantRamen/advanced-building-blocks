a3f0b2cf2403ca944d6316dc14bde043fab26bb8
Derrick Cudd <instantramen125@gmail.com>
2017-03-14 04:05:19 +0000

    finish bubble_sort_by

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-array_to_sort = [10,3,2,4,9,8,5,7,6,1,4,"a"]
+array_to_sort = [10,3,2,4,9,8,5,7,6,1,4]
 def bubble_sort (array)
   loop do
     number_swapped = 0
@@ -13,8 +13,19 @@ def bubble_sort (array)
   array
 end
 
-def bubble_sort_by
-    
+def bubble_sort_by (array)
+  loop do
+    number_swapped = 0
+    array.each_with_index do |number, i|
+      sort_by = yield(array[i], array[i + 1]) if array[i + 1]
+      if(array[i + 1] and sort_by > 0)
+        swap(array, i, i + 1)
+        number_swapped += 1
+      end
+    end
+    break if number_swapped == 0
+  end
+  array
 end
 
 def swap(array, index_1, index_2)
@@ -23,3 +34,8 @@ def swap(array, index_1, index_2)
 end
 
 puts bubble_sort(array_to_sort).to_s
+
+new = bubble_sort_by (["hi","hello","hey", "this is the longest one", "a"]) do |left,right|
+  right.length - left.length
+end
+puts new

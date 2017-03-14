43b235ccafee49864e04769f434f87216b88c4c9
Derrick Cudd <instantramen125@gmail.com>
2017-03-14 04:21:07 +0000

    clean code and make it more DRY

--- main.rb
+++ main.rb
@@ -1,18 +1,3 @@
-array_to_sort = [10,3,2,4,9,8,5,7,6,1,4]
-def bubble_sort (array)
-  loop do
-    number_swapped = 0
-    array.each_with_index do |number, i|
-      if(array[i + 1] && array[i] > array[i + 1])
-        array = swap(array, i, i + 1)
-        number_swapped += 1
-      end
-    end
-    break if number_swapped == 0
-  end
-  array
-end
-
 def bubble_sort_by (array)
   loop do
     number_swapped = 0
@@ -28,12 +13,17 @@ def bubble_sort_by (array)
   array
 end
 
+def bubble_sort (array)
+  bubble_sort_by(array) do |left, right|
+    left - right
+  end
+end
 def swap(array, index_1, index_2)
   array[index_1], array[index_2] = array[index_2], array[index_1]
   array
 end
 
-puts bubble_sort(array_to_sort).to_s
+puts bubble_sort([10,3,2,4,9,8,5,7,6,1,4]).to_s
 
 new = bubble_sort_by (["hi","hello","hey", "this is the longest one", "a"]) do |left,right|
   right.length - left.length
